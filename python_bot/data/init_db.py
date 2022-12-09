ff9e16923932abad475f84ca258f8b2a4ffbf5c1
schuub <34111742+schuub@users.noreply.github.com>
2022-12-09 06:03:24 +0100

    [MOV] rename

--- python_bot/data/init_db.py
+++ python_bot/data/init_db.py
@@ -5,8 +5,8 @@ import python_bot.data.models as models
 from python_bot.data import initial_data
 
 
-def main():
-    init_db()
+def main(filename: str):
+    init_db(filename=filename)
     session = db_session.create_session()
     if session.query(models.ChannelType).count() > 0:
         print("Nothing inserted: Table 'ChannelType' is not empty")
@@ -21,12 +21,16 @@ def insert_rows(rows: list):
     session.commit()
 
 
-def init_db():
+def init_db(filename: str) -> None:
     top_folder = os.path.dirname(__file__)
-    rel_file = os.path.join("..", "db", "config.sqlite")
+    rel_file = os.path.join("..", "db", filename)
     db_file = os.path.abspath(os.path.join(top_folder, rel_file))
     db_session.global_init(db_file)
 
 
+def init_test_db():
+    init_db("test.sqlite")
+
+
 if __name__ == '__main__':
-    main()
+    main("config.sqlite")
