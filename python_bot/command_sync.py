aaec038b24fc4ab14f392b9b87787040e71acf24
schuub <34111742+schuub@users.noreply.github.com>
2022-12-12 09:53:44 +0100

    [ADD] clear commands script

--- python_bot/command_sync.py
+++ python_bot/command_sync.py
@@ -1,6 +1,37 @@
-# TODO extract syncing of commands out of client setup, so it does not get called everytime the bot is started
-# commands only need to be synced when command parameters have changed
+# Clear all commands
+
+# TODO sync commands only on special command where the bot has to be mentioned, also check owner id
+import os
+
+import discord
+from discord import app_commands
+from dotenv import load_dotenv
+
+
+class MyClient(discord.Client):
+    def __init__(self) -> None:
+        intents = discord.Intents.default()
+        super().__init__(intents=intents)
+
+        self.tree = app_commands.CommandTree(self)
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        print("------")
+        bot_owner = await client.fetch_user(int(os.getenv("BOT_OWNER")))
+        await bot_owner.send("Bot commands cleared")
+
+    async def setup_hook(self) -> None:
+        print(f"Clear commands for {TEST_GUILD}")
+        self.tree.clear_commands(guild=TEST_GUILD)
+
+        print(f"Sync commands for {TEST_GUILD}")
+        await self.tree.sync(guild=TEST_GUILD)
+        # await self.tree.sync()
 
 
 if __name__ == '__main__':
-    pass
+    load_dotenv()
+    TEST_GUILD = discord.Object(int(os.getenv("TEST_GUILD")))
+    client = MyClient()
+    client.run(token=os.getenv("DISCORD_TOKEN"), reconnect=False)
